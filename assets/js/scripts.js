e2a080264269078ee48eb883c4eb2b89ec249582
John Walling <j00walling@gmail.com>
2021-08-20 22:54:10 -0700

    added authorization

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,92 @@
-const secret_data = JSON.parse('assets/secret/config.json');
+// var request = new XMLHttpRequest();
+// request.open("GET", "assets/json/config.json", false);
+// request.send(null);
+
+// const client_data = JSON.parse(JSON.parse(request.responseText));
+// console.log(client_data);
 
 const redirect_uri = "https://j00walling.github.io/john-walling/";
-const client_id = secret_data[0].CLIENT_ID;
-const client_secret = secret_data[0].CLIENT_SECRET;
-console.log(client_id);
-console.log(client_secret);
+const client_id = config.CLIENT_ID;
+const client_secret = config.CLIENT_SECRET;
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQA1ULpF6fdtkSesAtjxTIKrj_vYHzX3U1HaFraJK-siSbZVsm9Ud44WHxXIpLzwcE26fOqKf7t1KGFEqlkAXl20elk7NqTjQqtgqnNgldcgxszPUglO8J2xeV3QBq6oQxQ2KfOjHLHwdFwhu0psSYk3AHMCmdTco_gy'
+  function onPageLoad() {
+    if ( window.location.search.length > 0 ){
+        handleRedirect();
+    }
+    else{
+        access_token = localStorage.getItem("access_token");
+        if ( access_token == null ){
+            // we don't have an access token so present token section
+            document.getElementById("tokenSection").style.display = 'block';  
+        }
+        else {
+            // we have an access token so present device section
+            document.getElementById("deviceSection").style.display = 'block';  
+            refreshDevices();
+            refreshPlaylists();
+            currentlyPlaying();
+        }
+    }
+    refreshRadioButtons();
+  }
+  
+  function handleRedirect(){
+    let code = getCode();
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); // remove param from url
+  }
+  
+  function getCode(){
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+  }
+  
+  function fetchAccessToken( code ){
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+  }
+  
+  function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://accounts.spotify.com/api/token", true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+  }
+  
+  function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+  }
+  
+    const token = localStorage.getItem("access_token")
     const player = new Spotify.Player({
       name: 'John\'s Website',
       getOAuthToken: cb => { cb(token); }
@@ -36,9 +115,16 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     player.connect();
 
     $('.playback').on('click', function() {
-        player.togglePlay().then(() => {
-            console.log('Toggled playback!');
-        });
+        // player.togglePlay().then(() => {
+        //     console.log('Toggled playback!');
+        // });
+        let url = "https://accounts.spotify.com/authorize"
+        url += "?client_id=" + client_id;
+        url += "&response_type=code";
+        url += "&redirect_uri=" + encodeURI(redirect_uri);
+        url += "&show_dialog=true";
+        url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+        window.location.href = url; // Show Spotify's authorization screen
     })
   };
 
