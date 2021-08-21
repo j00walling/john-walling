8cc7b8b2b441bcd7092ed21e22635dd3fedb069d
John Walling <j00walling@gmail.com>
2021-08-20 22:56:24 -0700

    updated auth

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,83 +9,83 @@ const redirect_uri = "https://j00walling.github.io/john-walling/";
 const client_id = config.CLIENT_ID;
 const client_secret = config.CLIENT_SECRET;
 
+function onPageLoad() {
+  if ( window.location.search.length > 0 ){
+      handleRedirect();
+  }
+  else{
+      access_token = localStorage.getItem("access_token");
+      if ( access_token == null ){
+          // we don't have an access token so present token section
+          document.getElementById("tokenSection").style.display = 'block';  
+      }
+      else {
+          // we have an access token so present device section
+          document.getElementById("deviceSection").style.display = 'block';  
+          refreshDevices();
+          refreshPlaylists();
+          currentlyPlaying();
+      }
+  }
+  refreshRadioButtons();
+}
+
+function handleRedirect(){
+  let code = getCode();
+  fetchAccessToken( code );
+  window.history.pushState("", "", redirect_uri); // remove param from url
+}
+
+function getCode(){
+  let code = null;
+  const queryString = window.location.search;
+  if ( queryString.length > 0 ){
+      const urlParams = new URLSearchParams(queryString);
+      code = urlParams.get('code')
+  }
+  return code;
+}
+
+function fetchAccessToken( code ){
+  let body = "grant_type=authorization_code";
+  body += "&code=" + code; 
+  body += "&redirect_uri=" + encodeURI(redirect_uri);
+  body += "&client_id=" + client_id;
+  body += "&client_secret=" + client_secret;
+  callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body){
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://accounts.spotify.com/api/token", true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+  xhr.send(body);
+  xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse(){
+  if ( this.status == 200 ){
+      var data = JSON.parse(this.responseText);
+      console.log(data);
+      var data = JSON.parse(this.responseText);
+      if ( data.access_token != undefined ){
+          access_token = data.access_token;
+          localStorage.setItem("access_token", access_token);
+      }
+      if ( data.refresh_token  != undefined ){
+          refresh_token = data.refresh_token;
+          localStorage.setItem("refresh_token", refresh_token);
+      }
+      onPageLoad();
+  }
+  else {
+      console.log(this.responseText);
+      alert(this.responseText);
+  }
+}
+
 window.onSpotifyWebPlaybackSDKReady = () => {
-  function onPageLoad() {
-    if ( window.location.search.length > 0 ){
-        handleRedirect();
-    }
-    else{
-        access_token = localStorage.getItem("access_token");
-        if ( access_token == null ){
-            // we don't have an access token so present token section
-            document.getElementById("tokenSection").style.display = 'block';  
-        }
-        else {
-            // we have an access token so present device section
-            document.getElementById("deviceSection").style.display = 'block';  
-            refreshDevices();
-            refreshPlaylists();
-            currentlyPlaying();
-        }
-    }
-    refreshRadioButtons();
-  }
-  
-  function handleRedirect(){
-    let code = getCode();
-    fetchAccessToken( code );
-    window.history.pushState("", "", redirect_uri); // remove param from url
-  }
-  
-  function getCode(){
-    let code = null;
-    const queryString = window.location.search;
-    if ( queryString.length > 0 ){
-        const urlParams = new URLSearchParams(queryString);
-        code = urlParams.get('code')
-    }
-    return code;
-  }
-  
-  function fetchAccessToken( code ){
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code; 
-    body += "&redirect_uri=" + encodeURI(redirect_uri);
-    body += "&client_id=" + client_id;
-    body += "&client_secret=" + client_secret;
-    callAuthorizationApi(body);
-  }
-  
-  function callAuthorizationApi(body){
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://accounts.spotify.com/api/token", true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
-    xhr.send(body);
-    xhr.onload = handleAuthorizationResponse;
-  }
-  
-  function handleAuthorizationResponse(){
-    if ( this.status == 200 ){
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-        var data = JSON.parse(this.responseText);
-        if ( data.access_token != undefined ){
-            access_token = data.access_token;
-            localStorage.setItem("access_token", access_token);
-        }
-        if ( data.refresh_token  != undefined ){
-            refresh_token = data.refresh_token;
-            localStorage.setItem("refresh_token", refresh_token);
-        }
-        onPageLoad();
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-  }
-  
     const token = localStorage.getItem("access_token")
     const player = new Spotify.Player({
       name: 'John\'s Website',
