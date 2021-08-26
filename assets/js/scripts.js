8a3f4d4425b92eb9b7c770e2ce27fe051de1240b
John Walling <j00walling@gmail.com>
2021-08-25 19:03:20 -0700

    fixed spotify thing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,29 +5,41 @@
 // const client_data = JSON.parse(JSON.parse(request.responseText));
 // console.log(client_data);
 
-const redirect_uri = "https://j00walling.github.io/john-walling/";
+const redirect_uri = "http://127.0.0.1:5500/index.html";
 const client_id = config.CLIENT_ID;
 const client_secret = config.CLIENT_SECRET;
 
+var URIS = {};
+var SONG_LIST = null;
+var CURRENT_TRACK = null;
+var CURRENT_ARTIST = null;
+var BODY = [];
+
 function onPageLoad() {
-  if ( window.location.search.length > 0 ){
+  if (window.location.search.length > 0) {
       handleRedirect();
   }
-  else{
+  else {
       access_token = localStorage.getItem("access_token");
       if ( access_token == null ){
-          // we don't have an access token so present token section
-          document.getElementById("tokenSection").style.display = 'block';  
+        let url = "https://accounts.spotify.com/authorize"
+        url += "?client_id=" + client_id;
+        url += "&response_type=code";
+        url += "&redirect_uri=" + encodeURI(redirect_uri);
+        url += "&show_dialog=true";
+        url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+        window.location.href = url; // Show Spotify's authorization screen
       }
-      else {
-          // we have an access token so present device section
-          document.getElementById("deviceSection").style.display = 'block';  
-          refreshDevices();
-          refreshPlaylists();
-          currentlyPlaying();
-      }
+      getPlaylist();
   }
-  refreshRadioButtons();
+}
+
+function refreshAccessToken(){
+  refresh_token = localStorage.getItem("refresh_token");
+  let body = "grant_type=refresh_token";
+  body += "&refresh_token=" + refresh_token;
+  body += "&client_id=" + client_id;
+  callAuthorizationApi(body);
 }
 
 function handleRedirect(){
@@ -85,24 +97,106 @@ function handleAuthorizationResponse(){
   }
 }
 
+function callApi(method, url, body, callback){
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+  xhr.send(body);
+  xhr.onload = callback;
+}
+
+function handleApiResponse(){
+  if ( this.status == 200){
+      console.log(this.responseText);
+      // setTimeout(currentlyPlaying, 2000);
+  }
+  else if ( this.status == 204 ){
+      // setTimeout(currentlyPlaying, 2000);
+  }
+  else if ( this.status == 401 ){
+      refreshAccessToken();
+  }
+  else {
+      console.log(this.responseText);
+      onPageLoad();
+  }    
+}
+
+// function getRecentlyPlayed(){
+//   callApi("GET", "https://api.spotify.com/v1/me/player/recently-played", null, handleRecentlyPlayed);
+// }
+
+// function handleRecentlyPlayed(){
+//   if ( this.status == 200 ){
+//     songList = JSON.parse(this.responseText);
+//   }
+//   else if ( this.status == 401 ){
+//       refreshAccessToken()
+//   }
+//   else {
+//       console.log(this.responseText);
+//       onPageLoad();
+//   }
+// }
+
+function play(DEVICE_ID, URIS) {
+  callApi("PUT", "https://api.spotify.com/v1/me/player/play?device_id=" + DEVICE_ID, URIS, handleApiResponse);
+}
+
+function pause(DEVICE_ID) {
+  callApi("PUT", "https://api.spotify.com/v1/me/player/pause?device_id=" + DEVICE_ID, null, handleApiResponse);
+}
+
+// function currentlyPlaying() {
+//   callApi("GET", "https://api.spotify.com/v1/me/player/currently-playing", null, handleCurrentlyPlaying);
+// }
+
+function skip(DEVICE_ID) {
+  callApi("POST", "https://api.spotify.com/v1/me/player/next?device_id=" + DEVICE_ID, null, handleApiResponse);
+}
+
+function getPlaylist() {
+  callApi("GET", "https://api.spotify.com/v1/playlists/0Me1ANsc8Kmz1cUNdpLK3v", null, handleGetPlaylist);
+}
+
+function handleGetPlaylist() {
+  if (this.status == 200) {
+    SONG_LIST = JSON.parse(this.responseText);
+  }
+  else {
+    console.log(this.responseText);
+  }
+}
+
+// function handleCurrentlyPlaying() {
+//   if (this.status == 200) { // Something is playing
+//     var data = JSON.parse(this.responseText);
+
+//     if ( data.item != null ){
+//       document.getElementById("trackTitle").innerHTML = data.item.name;
+//       document.getElementById("trackArtist").innerHTML = data.item.artists[0].name;
+//     }
+//   }
+//   else if (this.status == 401) {
+//     refreshAccessToken();
+//   }
+//   else {
+//     console.log(this.responseText);
+//   }
+// }
+
 window.onSpotifyWebPlaybackSDKReady = () => {
     const token = localStorage.getItem("access_token")
+    var DEVICE_ID;
     const player = new Spotify.Player({
       name: 'John\'s Website',
       getOAuthToken: cb => { cb(token); }
     });
   
-    // Error handling
-    player.addListener('initialization_error', ({ message }) => { console.error(message); });
-    player.addListener('authentication_error', ({ message }) => { console.error(message); });
-    player.addListener('account_error', ({ message }) => { console.error(message); });
-    player.addListener('playback_error', ({ message }) => { console.error(message); });
-  
-    // Playback status updates
-    player.addListener('player_state_changed', state => { console.log(state); });
-  
     // Ready
     player.addListener('ready', ({ device_id }) => {
+      DEVICE_ID = device_id;
       console.log('Ready with Device ID', device_id);
     });
   
@@ -110,23 +204,64 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     player.addListener('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id);
     });
+
+    player.addListener('player_state_changed', ({
+      position,
+      duration,
+      track_window: { current_track }
+    }) => { 
+      document.getElementById("trackTitle").innerHTML = current_track.name;
+      document.getElementById("by").innerHTML = " - ";
+      
+      let temp = "";
+      for (let i = 0; i < current_track.artists.length; i++) {
+        temp += current_track.artists[i].name;
+        temp += ", ";
+      }
+
+      artists = temp.slice(0, -2);
+      
+      document.getElementById("trackArtist").innerHTML = artists;
+    });
   
     // Connect to the player!
-    player.connect();
-
-    $('.playback').on('click', function() {
-        // player.togglePlay().then(() => {
-        //     console.log('Toggled playback!');
-        // });
-        let url = "https://accounts.spotify.com/authorize"
-        url += "?client_id=" + client_id;
-        url += "&response_type=code";
-        url += "&redirect_uri=" + encodeURI(redirect_uri);
-        url += "&show_dialog=true";
-        url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
-        window.location.href = url; // Show Spotify's authorization screen
+    player.connect().then(success => {
+      if (success) {
+        console.log('The Web Playback SDK successfully connected to Spotify!');
+        console.log(SONG_LIST);
+        for (let i = 0; i < SONG_LIST.tracks.items.length; i++) {
+          BODY[i] = SONG_LIST.tracks.items[i].track.uri;
+        }
+        URIS.uris = BODY;
+      }
     })
-  };
+  
+    $('.playback').on('click', function() {
+      pic = document.getElementsByClassName("playback");
+      
+      if (pic[0].src == "http://127.0.0.1:5500/assets/img/iconmonstr-video-15-240.png") {
+        pic[0].src = "http://127.0.0.1:5500/assets/img/iconmonstr-media-control-8-240.png";
+
+        if (document.getElementById("by").innerHTML == "") {
+          play(DEVICE_ID, JSON.stringify(URIS));
+        }
+        else {
+          callApi("PUT", "https://api.spotify.com/v1/me/player/play?device_id=" + DEVICE_ID, null, handleApiResponse);
+        }
+      }
+      else {
+        pic[0].src = "http://127.0.0.1:5500/assets/img/iconmonstr-video-15-240.png";
+        pause(DEVICE_ID);
+      } 
+    });
+
+    $('.skip').on('click', function() {
+      skip(DEVICE_ID);
+      if (pic[0].src == "http://127.0.0.1:5500/assets/img/iconmonstr-video-15-240.png") {
+        pic[0].src = "http://127.0.0.1:5500/assets/img/iconmonstr-media-control-8-240.png";
+      }
+    });
+  }
 
 function scroll_to(clicked_link, nav_height) {
     var element_class = clicked_link.attr('href').replace('#', '.');
@@ -148,5 +283,4 @@ jQuery(document).ready(function() {
     });
 
     $('.top-container').backstretch("assets/img/mountain.png");
-    
-});
+  });
